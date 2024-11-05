add96d7115fb20f3fc31d24d11f16aa2e722b17e
Each9084 <each9084@gmail.com>
2024-11-05 21:39:21 +0000

    new input_page.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  
   runApp(BMICalculator());
 }
 
@@ -12,16 +11,16 @@ class BMICalculator extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           primaryColor: Colors.red,
-          colorScheme:
-          ColorScheme.fromSwatch().copyWith(
-              primary: Colors.red,
-              secondary: Colors.green,
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Color(0xFF0A0D22),
+            secondary: Color(0xfFF0A0D22),
           ),
+          scaffoldBackgroundColor: Color(0xfFF0A0D22),
+          textTheme: TextTheme(
+              bodyMedium: TextStyle(color: Colors.white, fontSize: 20)),
           floatingActionButtonTheme: FloatingActionButtonThemeData(
             backgroundColor: Colors.purple,
-          )
-      ),
-
+          )),
       home: InputPage(),
     );
   }
@@ -37,13 +36,14 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
+        title: Center(child: const Text('BMI CALCULATOR')),
         backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
       ),
-      body: const Center(
-        child: const Text('Body Text'),
-
+      body: Center(
+        child: Text('Body Text', style: Theme.of(context).textTheme.bodyMedium),
       ),
+      /*backgroundColor: Theme.of(context).colorScheme.secondary,*/
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
